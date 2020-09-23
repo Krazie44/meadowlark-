8b852b41a89b1d29ef34c3a8384af19cd2b196c3
Douglas Keeton <keetod5597@clarkstate.edu>
2020-09-23 17:14:34 -0400

    Initial commit.

---
+++ meadowlark.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+	defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+
+const fortunes = [
+  "What goes around, comes around.",
+  "Karma is a biatch!",
+  "If you play nice with others, they will play nice to you.",
+  "Be like Nike and Just Do It!",
+  "Whatever your desire, focus, and it will be yours.",
+]
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render('about', { fortune: randomFortune })
+})
+
+// custom 404 page
+app.use((req, res) => {
+  res.status(404)
+  res.render('404')
+})
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.status(500)
+  res.render('500')
+})
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`))
